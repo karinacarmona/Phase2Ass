074b5b3caaeeab70d1ec8157d8c16943db6d2e99
Karina Carmona <karinacarmona@me.com>
2014-12-03 20:52:57 -0500

    YAYYYY I CAN CREATE I AM A GOD

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :resources
   has_many :post_tags
-  has_many :tags, through: :post_tagss
+  has_many :tags, through: :post_tags
 
   validates :title, :presence => true
   validates :content, :presence => true
